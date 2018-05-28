51127091187683450dc2e2768375d07464cf918b
unknown <leelasantoshi@gmail.com>
2018-05-28 11:38:15 -0500

    this is a new addition class

---
+++ src/main/java/com/acme/hashset/Addition.java
@@ -0,0 +1,9 @@
+package com.acme.hashset;
+
+public class Addition {
+public static void getSumOfNumbers(int x, int y)
+{
+	int z = x+y;
+	System.out.println(z);
+}
+}
---
+++ src/test/java/com/acme/testSumOfNumbers.java
@@ -0,0 +1,16 @@
+package com.acme;
+
+import org.junit.Test;
+
+import com.acme.hashset.Addition;
+
+public class testSumOfNumbers {
+	@Test
+	public void testSumOfNumbers()
+	{
+		int x = 2;
+		int y = 3;
+		Addition.getSumOfNumbers(x, y);
+	}
+
+}
